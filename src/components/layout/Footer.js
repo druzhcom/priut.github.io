f002eae402fa5fde759b721dc7923d0c8a48ca0c
Ilya Druzhinin <ilya.druzh@gmail.com>
2022-10-03 11:17:10 +0300

    update links

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -52,7 +52,7 @@ const Footer = ({
           </div>
           <div className="footer-bottom space-between text-xxs invert-order-desktop">
             <FooterNav />
-            <div className="footer-copyright">Сделано <a href="#">ООО "Дружининъ"</a> {" "} </div>
+            <div className="footer-copyright">Сделано <a href="https://druzh.tech">ООО "Дружининъ"</a> {" "} </div>
 
           </div>
         </div>
